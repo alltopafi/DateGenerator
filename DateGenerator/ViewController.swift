9199bbe7dcb02b730cb43b50eee4ea9cc0b341e4
alltopafi <alltopafi@gmail.com>
2017-01-26 09:22:15 -0600

    Initial Commit

---
+++ DateGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DateGenerator
+//
+//  Created by Jesse Alltop on 1/26/17.
+//  Copyright © 2017 Jesse Alltop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
